6ad42653b616e816cc43ded421717f86ecfd162e
Élise <elise@eiss.fr>
2023-06-05 16:57:27 +0200

    lunasnip errors so i disabled it

--- lua/plugins/lsp_zero.lua
+++ lua/plugins/lsp_zero.lua
@@ -32,7 +32,7 @@ return { {
 		{ 'williamboman/mason-lspconfig.nvim' },
 		{ 'hrsh7th/nvim-cmp' },
 		{ 'hrsh7th/cmp-nvim-lsp' },
-		{ 'L3MON4D3/LuaSnip' },
+		{ 'L3MON4D3/LuaSnip', enabled = false },
 		{ "j-hui/fidget.nvim" },
 		{
 			"folke/neodev.nvim",

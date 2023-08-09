467464642800900c3b79bedd6a0be2ac6ffae609
Élise <elise@eiss.fr>
2023-08-09 10:13:46 +0200

    meow

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -150,7 +150,16 @@ return {
 		lazy = false,
 		requires = { "nvim-tree/nvim-web-devicons" },
 		config = function()
-			require("alpha").setup(require("alpha.themes.startify").config)
+			local startify = require("alpha.themes.startify")
+			startify.section.header.val = {
+[[                                    (_)                                      ]],
+[[ _ __ ___   ___  _____      ____   ___ _ __ ___       |\      _,,,---,,_     ]],
+[[| '_ ` _ \ / _ \/ _ \ \ /\ / /\ \ / / | '_ ` _ \  Z zz /,`.-'`'    -.  ;-;;,_]],
+[[| | | | | |  __/ (_) \ V  V /  \ V /| | | | | | |    |,4-  ) )-,_. ,\ (  `'-']],
+[[|_| |_| |_|\___|\___/ \_/\_/    \_/ |_|_| |_| |_|   '---''(_/--'  `-'\_)     ]],
+			}
+			require("alpha").setup(startify.config)
+
 		end,
 		keys = {
 			{ "<leader>a", ":Alpha<cr>", desc = "dashboard" },

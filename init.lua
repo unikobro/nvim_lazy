e53b4d7840e9ba3ddf276c3568a0fdb743916b1c
Élise <elise@eiss.fr>
2023-10-03 11:59:15 +0200

    nvim reset cursor shape

--- init.lua
+++ init.lua
@@ -14,6 +14,9 @@ else
 		vim.g.neovide_transparency = 0.9
 		vim.g.neovide_floating_blur_amount_x = 2.0
 		vim.g.neovide_floating_blur_amount_y = 2.0
+	else
+		vim.o.guicursor = "n-v-c-sm:block,i-ci-ve:ver25,r-cr-o:hor20"
+		vim.cmd("au VimLeave * set guicursor=a:hor10-blinkon0")
 	end
 	-- ordinary Neovim
 end

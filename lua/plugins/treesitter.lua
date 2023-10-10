0b4df19c3dbc1f7d78b3172943f22ca139934702
Élise <elise@eiss.fr>
2023-10-10 11:33:29 +0200

    disabling syntaxt highlighting for git commit and sql

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -19,6 +19,12 @@ local function config_ts()
 
 			-- Or use a function for more flexibility, e.g. to disable slow treesitter highlight for large files
 			disable = function(lang, buf)
+				if (lang == "gitcommit") then
+					return true
+				end
+				if (lang == "sql") then
+					return true
+				end
 				local max_filesize = 100 * 1024 -- 100 KB
 				local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
 				if ok and stats and stats.size > max_filesize then return true end

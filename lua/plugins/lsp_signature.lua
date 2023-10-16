f72c499e55af0f8c52f1e9f243e718c9cab5cb99
Élise <elise@eiss.fr>
2023-10-16 13:23:31 +0200

    LSP sig used to show on line currently editing
    - removed borders
    - fixed positioning

--- lua/plugins/lsp_signature.lua
+++ lua/plugins/lsp_signature.lua
@@ -9,11 +9,12 @@ return { {
 			hint_prefix = "",
 			hint_enable = false,
 			doc_lines = 0,
-			floating_window_off_x = 0,
---			handler_opts = {
---				border = "rounded",
---			},
-			floating_window_above_cur_line = false,
+			-- floating_window_off_x = 10,
+			handler_opts = {
+				border = "none",
+			},
+			-- floating_window_above_cur_line = false,
+			toggle_key = '<M-x>'
 		})
 	end
 } }

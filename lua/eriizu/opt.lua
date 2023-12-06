bb4fea98728163f7554db39574ce2f1698a9759c
Élise <elise@eiss.fr>
2023-12-06 12:50:15 +0100

    Leave insert properly C-c, dirty leave still possible using qq

--- lua/eriizu/opt.lua
+++ lua/eriizu/opt.lua
@@ -33,4 +33,11 @@ vim.keymap.set("n", "<leader>tx", ":tabclose<cr>", { desc = "tab close", silent 
 
 vim.keymap.set("n", "<M-right>", ":tabnext<cr>", { desc = "tab next", silent = true })
 vim.keymap.set("n", "<M-left>", ":tabprevious<cr>", { desc = "tab previous", silent = true })
+
+-- INFO: I want C-c to execute autocmd InsertLeave, as I'm addicted to C-c on everything
+-- and would love highlighters to still be able to do their job.
+-- I'm still putting in an exit strategy if this causes problems,
+-- you want still qq to get the original C-c behaviour.
 vim.keymap.set("i", "jj", "<Esc>", { silent = true });
+vim.keymap.set("i", "qq", "<C-c>", { silent = false });
+vim.keymap.set("i", "<C-c>", "<Esc>", { silent = true });

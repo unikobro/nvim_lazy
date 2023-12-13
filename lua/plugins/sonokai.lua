191fc4cd407569b6d8f810d5572496d01abd8804
Élise <elise@eiss.fr>
2023-12-13 22:51:43 +0100

    trying on gruvbox

--- lua/plugins/sonokai.lua
+++ lua/plugins/sonokai.lua
@@ -1,12 +1,30 @@
-return { {
-	"sainnhe/sonokai",
-	lazy = false,
-	config = function()
-		vim.g.sonokai_style = "espresso"
-		vim.g.sonokai_transparent_background = 2
-		vim.g.sonokai_better_performance = 1
-		vim.g.sonokai_dim_inactive_windows = 0
-		vim.opt.termguicolors = true
-		vim.cmd([[colorscheme sonokai]])
-	end
-} }
+return {
+	{
+		"sainnhe/gruvbox-material",
+		lazy = false,
+		enabled = true,
+		config = function()
+
+			vim.g.gruvbox_material_background = "medium"
+			vim.g.gruvbox_material_foreground = "material"
+			vim.g.gruvbox_material_transparent_background = 0
+			vim.g.gruvbox_material_better_performance = 1
+			vim.g.gruvbox_material_dim_inactive_windows = 0
+			vim.opt.termguicolors = true
+			vim.cmd([[colorscheme gruvbox-material]])
+		end
+	},
+	{
+		"sainnhe/sonokai",
+		lazy = false,
+		enabled = false,
+		config = function()
+			vim.g.sonokai_style = "espresso"
+			vim.g.sonokai_transparent_background = 2
+			vim.g.sonokai_better_performance = 1
+			vim.g.sonokai_dim_inactive_windows = 0
+			vim.opt.termguicolors = true
+			vim.cmd([[colorscheme sonokai]])
+		end
+	}
+}

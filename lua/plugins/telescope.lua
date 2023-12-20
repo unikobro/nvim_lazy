525a0c0f4410ddd78ce227a9b040f852e80741a8
Élise <elise@eiss.fr>
2023-12-20 09:36:41 +0100

    updated telescope

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -37,7 +37,7 @@ end
 return { {
 	"nvim-telescope/telescope.nvim",
 	lazy = false,
-	tag = "0.1.4",
+	tag = "0.1.5",
 	dependencies = { 'nvim-lua/plenary.nvim', "nvim-telescope/telescope-ui-select.nvim" },
 	config = config_telescope,
 	keys = {

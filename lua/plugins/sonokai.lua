e9d45f655e0b3ccb281fd14fffc13ff59195b1bc
Élise <elise@eiss.fr>
2024-01-01 14:26:12 +0100

    gruvbox material now uses custom theme

--- lua/plugins/sonokai.lua
+++ lua/plugins/sonokai.lua
@@ -1,15 +1,17 @@
 return {
 	{
-		"sainnhe/gruvbox-material",
+		"eriizu/gruvbox-material",
+		branch = "custom_v2",
 		lazy = false,
 		enabled = true,
 		config = function()
-
 			vim.g.gruvbox_material_background = "medium"
 			vim.g.gruvbox_material_foreground = "material"
 			vim.g.gruvbox_material_transparent_background = 1
 			vim.g.gruvbox_material_better_performance = 1
 			vim.g.gruvbox_material_dim_inactive_windows = 0
+			vim.g.gruvbox_material_enable_italic = 1
+			vim.g.gruvbox_material_enable_bold = 0
 			vim.opt.termguicolors = true
 			vim.cmd([[colorscheme gruvbox-material]])
 		end

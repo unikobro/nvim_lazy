b6f1e3a991a66270e377fe0343c6288e0909046c
Élise <elise@eiss.fr>
2024-02-26 12:43:17 +0100

    in theme colors for todo-comments

--- lua/plugins/todo-comments.lua
+++ lua/plugins/todo-comments.lua
@@ -5,8 +5,18 @@ return {
 		"folke/todo-comments.nvim",
 		lazy = false,
 		dependencies = { "nvim-lua/plenary.nvim" },
-		config = function()
-			require("todo-comments").setup({})
-		end,
+		opts = {
+			colors = {
+				error = { "DiagnosticError", "ErrorMsg", "#ea6962" },
+				warning = { "DiagnosticWarn", "WarningMsg", "#d8a657" },
+				info = { "DiagnosticInfo", "#7daea3" },
+				hint = { "DiagnosticHint", "#89b482" },
+				default = { "Identifier", "#d3869b" },
+				test = { "Identifier", "#d3869b" }
+			},
+		},
+		-- config = function()
+		-- 	require("todo-comments").setup({})
+		-- end,
 	},
 }

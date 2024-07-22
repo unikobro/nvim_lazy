2f9057c9522d121f69601c3f33fbd0341e03a583
Élise <elise@eiss.fr>
2024-07-22 17:33:28 +0200

    disable clippy

--- lua/plugins/rust.lua
+++ lua/plugins/rust.lua
@@ -30,6 +30,13 @@ return {
 		"mrcjkb/rustaceanvim",
 		version = "^4", -- Recommended
 		lazy = false, -- This plugin is already lazy
-		ft = "rust",
+		config = function()
+			vim.g.rustaceanvim = {
+				tools = {
+					enable_clippy = false,
+				},
+			}
+		end,
+		-- ft = "rust",
 	},
 }

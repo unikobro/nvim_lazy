917d3d91cd418bc450263a43ee3aec4944baa724
Élise <elise@eiss.fr>
2024-11-07 11:12:32 +0100

    updated packages

--- lua/plugins/lsp_signature.lua
+++ lua/plugins/lsp_signature.lua
@@ -1,20 +1,21 @@
 -- INFO: shows signature for current function call
 -- tags: UI, IDE, LSP
-return { {
-	"ray-x/lsp_signature.nvim",
-	lazy= false,
-	--event = "LspAttach",
-	config = function()
-		require("lsp_signature").setup({
-			hint_prefix = "",
-			hint_enable = false,
-			doc_lines = 0,
-			-- floating_window_off_x = 10,
-			handler_opts = {
-				border = "none",
-			},
-			-- floating_window_above_cur_line = false,
-			toggle_key = '<M-x>'
-		})
-	end
-} }
+return {
+	{
+		"ray-x/lsp_signature.nvim",
+		lazy = false,
+		config = function()
+			require("lsp_signature").setup({
+				hint_prefix = "",
+				hint_enable = false,
+				doc_lines = 0,
+				-- floating_window_off_x = 10,
+				handler_opts = {
+					border = "none",
+				},
+				-- floating_window_above_cur_line = false,
+				toggle_key = "<M-x>",
+			})
+		end,
+	},
+}

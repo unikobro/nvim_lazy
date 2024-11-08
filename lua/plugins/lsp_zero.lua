6551dffdfb6f0773c076451ef1ca86b5f0b3ea95
Élise <elise@eiss.fr>
2024-11-08 11:50:55 +0100

    i think lsp_signature wants to be loaded before lsp, otherwise it can crash?

--- lua/plugins/lsp_zero.lua
+++ lua/plugins/lsp_zero.lua
@@ -71,6 +71,22 @@ return {
 					require("neodev").setup()
 				end,
 			},
+			{
+				"ray-x/lsp_signature.nvim",
+				config = function()
+					require("lsp_signature").setup({
+						hint_prefix = "",
+						hint_enable = false,
+						doc_lines = 0,
+						-- floating_window_off_x = 10,
+						handler_opts = {
+							border = "none",
+						},
+						-- floating_window_above_cur_line = false,
+						toggle_key = "<M-x>",
+					})
+				end,
+			},
 		},
 		keys = {
 			{
